5583c433ae611fc6b058d6e58039545cb40f4bb4
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2020-07-07 16:15:42 -0500

    built basic search component

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -3,11 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Header from "./components/Header";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Search />
     </div>
   );
 }
---
+++ weather-react/src/components/Search.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "../styles/search.scss";
+
+const Search = () => {
+  const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Search text: ${input}`);
+    setInput("");
+  };
+
+  return (
+    <div className="search">
+      <form>
+        <input type="text" value={input} onChange={handleChange} />
+        <button onClick={handleSubmit}>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
